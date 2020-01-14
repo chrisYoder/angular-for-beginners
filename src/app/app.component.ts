709a5ca7643b297898eb667451870f1b0ae9f54e
chrisYoder <christophermyoder@gmail.com>
2020-01-14 15:26:49 -0600

    5 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  
+  data = {
+    name: 'theCodingStoic',
+    started: '01.14.2020', 
+    course: 'Angular Core Deep Dive'
+  }
 }
--- src/app/hello.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  styles: [`h1 { font-family: Lato; }`]
-})
-export class HelloComponent  {
-  @Input() name: string;
-}
