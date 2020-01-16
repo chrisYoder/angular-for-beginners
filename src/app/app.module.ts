8bb91a5d81f9513f94aa50fc6526b08d78466ea3
chrisYoder <christophermyoder@gmail.com>
2020-01-16 12:55:01 -0600

    4 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ],
+  declarations: [ AppComponent, NavBarComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
